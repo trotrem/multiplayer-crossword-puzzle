8af8962fdf60f2c3708eb0527d46410f158a17c7
trotrem <jeanchristophe.buteau@gmail.com>
2018-01-25 18:50:11 -0500

    nettoyé le code de l'editeur

--- client/src/app/editeur/editeur.component.ts
+++ client/src/app/editeur/editeur.component.ts
@@ -1,14 +1,11 @@
 import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import * as THREE from 'three';
 
-
 @Component({
   selector: 'app-editeur',
   templateUrl: './editeur.component.html',
   styleUrls: ['./editeur.component.css']
 })
-
-
 export class EditeurComponent implements AfterViewInit {
 
   @ViewChild('canvas')
@@ -20,23 +17,15 @@ export class EditeurComponent implements AfterViewInit {
 
   private renderer: THREE.Renderer;
 
-
-
-
   private get canvas() : HTMLCanvasElement {
     return this.canvasRef.nativeElement;
   }
 
-
-
   constructor() { }
-
-
 
   ngAfterViewInit() {
     this.createScene();
     this.renderer.setSize( window.innerWidth, window.innerHeight );
-    this.camera.lookAt(this.scene.position);
     this.animate();
     this.canvas.addEventListener('click', (event) => this.onClick(event), false);
 
@@ -44,24 +33,25 @@ export class EditeurComponent implements AfterViewInit {
 
   createScene() {
     this.camera = new THREE.PerspectiveCamera(70,window.innerWidth/ window.innerHeight,0.1,1000 );
+    this.camera.lookAt(new THREE.Vector3(0,0,0));
+    this.camera.position.set(0,0,100);
     this.scene = new THREE.Scene();
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
   }
-
 
   animate() {
     requestAnimationFrame(() => this.animate());
     this.renderer.render(this.scene, this.camera);
   }
+
   createDot(position) {
     let geometry = new THREE.Geometry();
     geometry.vertices.push(position);
-    let material = new THREE.PointsMaterial({ size :1,color: 0x88d8b0 });
+    let material = new THREE.PointsMaterial({ color: 0x88d8b0 });
     let dot = new THREE.Points(geometry, material);
     this.scene.add( dot );
-    this.camera.position.z = 100;
-
   }
+
   convertToCanvasPosition(event){
     let vector = new THREE.Vector3((event.clientX / window.innerWidth)*2-1,-(event.clientY / window.innerHeight)*2+1 , 0.5);
     vector.unproject(this.camera);
@@ -69,11 +59,10 @@ export class EditeurComponent implements AfterViewInit {
     let distance = - this.camera.position.z / dir.z;
     return this.camera.position.clone().add(dir.multiplyScalar(distance));
   }
-  onClick(event) {
 
+  onClick(event) {
     let position = this.convertToCanvasPosition(event);
     console.log(position);
     this.createDot(position);
-
   }
 }
