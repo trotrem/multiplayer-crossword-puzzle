c3bff662d4336f45de9d8b8f44230696a529c614
Marc-Antoine De Serres <madeserres@videotron.ca>
2018-01-30 11:38:36 -0500

    Marc-Antoine : httpResquest de l'api externe

---
+++ server/app/routes/crossword/lexiconAPI/externalApi.service.ts
@@ -0,0 +1,24 @@
+import * as http from 'http'
+import { METHODS } from 'http';
+
+export class ExternalApiService {
+
+    // https://nodejs.org/api/http.html#http_http_request_options_callback
+    private static readonly API : http.RequestOptions = {
+        protocol : 'http',
+        // host : default
+        hostname : 'http://api.datamuse.com',
+        // family default IP v4 et v6
+        // port : 80 (default)
+        // localAdress ?
+        // socketPath : ...
+        method : 'GET',
+        // path : non utile
+        // headers : ?
+        // auth : pas besoin
+        agent : false // crée un nouveau par défaut
+        // createConnection : ...
+        // timeout : ...
+    }
+
+}
