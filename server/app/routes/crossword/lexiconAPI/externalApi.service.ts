905370189d9ffaf6b2aed0ad54f4225b157527a7
Marc-Antoine De Serres <madeserres@videotron.ca>
2018-01-31 16:07:36 -0500

    Marc-Antoine & JC : Query de l'api externe

--- server/app/routes/crossword/lexiconAPI/externalApi.service.ts
+++ server/app/routes/crossword/lexiconAPI/externalApi.service.ts
@@ -1,56 +1,33 @@
-import * as http from 'http'
-import { METHODS } from 'http';
+import * as requestOption from 'request-promise-native'
+
 
 export class ExternalApiService {
 
-    // https://nodejs.org/api/http.html#http_http_request_options_callback
-    private static readonly API: http.RequestOptions = {
-        protocol: 'http',
-        // host : default
-        hostname: 'http://api.datamuse.com',
-        // family default IP v4 et v6
-        // port : 80 (default)
-        // localAdress ?
-        // socketPath : ...
-        method: 'GET',
-        // path : non utile
-        // headers : ?
-        // auth : pas besoin
-        agent: false // crée un nouveau par défaut
-        // createConnection : ...
-        // timeout : ...
+    public options = {
+        method : 'GET',
+        uri : 'http://api.datamuse.com/words',
+        json : true,
+        qs : {
+            sp : 'h?ll',
+            md :'df', 
+        },
+        simple : true
     };
 
-    // https://basarat.gitbooks.io/typescript/docs/promise.html 
-    requestInfo(requestOptionsPath: http.RequestOptions): Promise<any> {
-        return new Promise((resolve, reject) => {
-            // Request avec le path
-            const PATH_API: http.RequestOptions = {
-                ...ExternalApiService.API,
-                ...requestOptionsPath
-            };
-            // https://nodejs.org/api/http.html#http_class_http_incomingmessage
-            const request = http.request(PATH_API);
-            request.on('response', (result: http.IncomingMessage) => {
-                let resp = '';
-                result.on('data', (chunk: string) => resp += chunk);
-                result.on('end', () => resolve(resp));
-                result.on('error', reject);
-            }).on('error', reject);
-            request.end();
 
+public request() {
+    requestOption(this.options)
+        .then(function (res:any) {
+            console.log(res[2].word);
+        })
+        .catch(function (err:any) {
+            // API call failed...
         });
-    }
-
-
-
-
-    public getDefinitions(word: string): Promise<string[]> {
-        return this.requestInfo({
-            path: `/words?sp=${word}&md=d&max=1`
-        }).then(resp) => {
-            const DEFINITION = JSON.parse(resp);
-        }
-    }
+}
 
 }
+
+
+let bleh = new ExternalApiService;
+bleh.request();
+//searchDefs()
