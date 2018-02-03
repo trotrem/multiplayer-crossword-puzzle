5eaa5a395b155592daecc1ed8f95c6060a2ecca0
Marc-Antoine De Serres <madeserres@videotron.ca>
2018-02-03 18:45:16 -0500

    Fonction wordHasDefinition possede un bogue

--- server/app/routes/crossword/lexiconAPI/externalApi.service.ts
+++ server/app/routes/crossword/lexiconAPI/externalApi.service.ts
@@ -1,9 +1,11 @@
 import * as requestOption from 'request-promise-native';
-//import { GridWordInformation } from './gridWordInformation';
+import { GridWordInformation } from './gridWordInformation';
 
 export class ExternalApiService {
 
     public requestResult: JSON;
+    public wordsWithDefinitions: GridWordInformation[];
+    public test: any;
 
     public options = {
         method: 'GET',
@@ -21,24 +23,39 @@ export class ExternalApiService {
         return requestOption(this.options)
             .then((result: JSON) => {
                 this.requestResult = result;
-
-                for (let index in this.requestResult) {
-                    if (!this.requestResult[index].hasOwnProperty('defs')) {
-                        delete this.requestResult[index];
-                    }
-                }
-
+                this.wordHasDefinition();
             }, reject => {
                 console.log('Rejected, Error...');
             })
             .catch((err: any) => { console.log('Erreur...') });
+    }
+
+    public getWordWithDefinition(): GridWordInformation[] {
+
+        return this.wordsWithDefinitions;
+    }
+
+    private wordHasDefinition(): void {
+        for (let index in this.requestResult) {
+            let indexDef: number = 0;
+            if (this.requestResult[index].hasOwnProperty('defs')) {
+               // let tempWord = new GridWordInformation(this.requestResult[index].word, this.requestResult[index].defs, 55)
+                //this.test = tempWord;
+                //this.wordsWithDefinitions[indexDef] = tempWord;
+                console.log('test...');
+                indexDef++;
+            }
+        }
+
     }
 }
 
 let bleh = new ExternalApiService;
 bleh.requestWordInfo('t?e')
     .then(() => {
-        console.log(bleh.requestResult[0]);
+        console.log(bleh.requestResult[0].frequency)
+        // console.log(bleh.wordsWithDefinitions[0].getWord());
     })
+    .catch((err: any) => { console.log('allo') });
 
 
