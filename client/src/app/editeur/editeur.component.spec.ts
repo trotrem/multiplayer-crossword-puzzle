9f99303a7eec961c8740a71d41690363be271ec8
Jean-Christophe Buteau <jeanchristophe.buteau@gmail.com>
2018-02-04 16:03:32 -0500

    JC - tslint for editeur specs

--- client/src/app/editeur/editeur.component.spec.ts
+++ client/src/app/editeur/editeur.component.spec.ts
@@ -1,13 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import * as THREE from 'three';
-import { EditeurComponent } from './editeur.component';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import * as THREE from "three";
+import { EditeurComponent } from "./editeur.component";
 
+// "magic numers" utilisés pour les tests
+/* tslint:disable:no-magic-numbers */
 
-describe('EditeurComponent', () => {
+describe("EditeurComponent", () => {
   let component: EditeurComponent;
   let fixture: ComponentFixture<EditeurComponent>;
-
-
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -22,33 +22,28 @@ describe('EditeurComponent', () => {
     fixture.detectChanges();
   });
 
-
-  it('should create the EditeurComponent ', () => {
-
+  it("should create the EditeurComponent", () => {
     expect(component).toBeDefined();
   });
-  it('should create a scene', () => {
 
+  it("should create a scene", () => {
     component.createScene();
     expect(component.getScene().children.length).toBe(0);
   });
 
-  it('should create a point', () => {
-
-    let position = new THREE.Vector3(-23,-2,0);
+  it("should create a point", () => {
+    const position: THREE.Vector3 = new THREE.Vector3(-23, -2, 0);
     component.createPoint(position);
     expect(component.getScene().children.length).toBe(1);
   });
 
-  it('should create a segement', () => {
-    let array = new Array<THREE.Vector3>();
-    let position1 = new THREE.Vector3(-23,-2,0);
+  it("should create a segment", () => {
+    const array: Array<THREE.Vector3> = new Array<THREE.Vector3>();
+    const position1: THREE.Vector3 = new THREE.Vector3(-23, -2, 0);
     array.push(position1);
     component.arrayPoints = array;
-    let position2 = new THREE.Vector3(23,-2,0);
-    component.createLine(position1,position2);
+    const position2: THREE.Vector3 = new THREE.Vector3(23, -2, 0);
+    component.createLine(position1, position2);
     expect(component.getScene().children.length).toBe(1);
   });
-
-
 });
