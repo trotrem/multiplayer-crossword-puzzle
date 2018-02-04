1938ff1d465af8fb8ff97e469b43bc7a82bbfb66
Marc-Antoine De Serres <madeserres@videotron.ca>
2018-02-04 02:27:49 -0500

    Assurance qualité du code du service lexical

--- server/app/routes/crossword/lexiconAPI/externalApi.service.ts
+++ server/app/routes/crossword/lexiconAPI/externalApi.service.ts
@@ -1,5 +1,7 @@
 import * as requestOption from "request-promise-native";
 import { GridWordInformation } from "./gridWordInformation";
+import { UriOptions } from "request";
+import { RequestPromiseOptions } from "request-promise";
 // import { Options } from "request";
 
 export class ExternalApiService {
@@ -11,7 +13,7 @@ export class ExternalApiService {
         this.wordsWithDefinitions = new Array<GridWordInformation>();
     }
 
-    public options: any = {
+    public readonly options: (UriOptions & RequestPromiseOptions) = {
         method: "GET",
         uri: "http://api.datamuse.com/words",
         json: true,
@@ -22,17 +24,16 @@ export class ExternalApiService {
         simple: true,
     };
 
-    public async requestWordInfo(word: string): Promise<any> {
+    public async requestWordInfo(word: string): Promise<void> { // Any == ?
         this.options.qs.sp = word;
 
-        return requestOption(this.options)
+        await requestOption(this.options)
             .then((result: JSON) => {
                 this.requestResult = result;
-                this.removeWordsWithoutDefinitions();
-            },(reject: any) => {
-                // console.log("Rejected, Error...");
+                this.createWordListWithDefinitions();
+                this.removeDefinitions();
             })
-            .catch((err: any) => { console.log("Erreur...") });
+            .catch((err: object) => { /* Erreur */ }); // ANy
     }
 
     public getWordsWithDefinitions(): GridWordInformation[] {
@@ -40,15 +41,42 @@ export class ExternalApiService {
         return this.wordsWithDefinitions;
     }
 
-    private removeWordsWithoutDefinitions(): void {
-        for (let index in this.requestResult) {
-            let wordHasDefinitions: boolean = this.requestResult[index].hasOwnProperty("defs")
-            if (wordHasDefinitions) {
-                let tempFrequency: string = this.requestResult[index].tags[0].substring(2);
-                let tempWord: GridWordInformation = new GridWordInformation
+    private createWordListWithDefinitions(): void {
+
+        for (const index in this.requestResult) {
+            if (this.requestResult[index].hasOwnProperty("defs")) {
+                const nonNumericalRemoval: number = 2;
+                const tempFrequency: string = this.requestResult[index].tags[0].substring(nonNumericalRemoval);
+                const tempWord: GridWordInformation = new GridWordInformation
                     (this.requestResult[index].word, this.requestResult[index].defs, parseFloat(tempFrequency));
                 this.wordsWithDefinitions.push(tempWord);
             }
         }
     }
+
+    private removeDefinitions(): void {
+
+        for (let indexWord: number = 0; indexWord < this.wordsWithDefinitions.length; indexWord++) {
+            for (let index: number = 0; index < this.wordsWithDefinitions[indexWord].getDefinitions().length; index++) {
+                if (!(this.wordsWithDefinitions[indexWord].getDefinitions()[0].charAt(0) === "n")
+                    && !(this.wordsWithDefinitions[indexWord].getDefinitions()[0].charAt(0) === "v")) {
+                    this.wordsWithDefinitions[indexWord].getDefinitions().splice(index, 1);
+                    index--; // next object is at same index as the one removed
+                }
+            }
+            if (this.wordsWithDefinitions[indexWord].getDefinitions().length === 0) {
+                this.wordsWithDefinitions.splice(indexWord, 1);
+                indexWord--; // next object is at same index as the one removed
+            }
+        }
+    }
+
 }
+
+/*let bleh: ExternalApiService = new ExternalApiService;
+bleh.requestWordInfo("t?e")
+    .then(() => {
+        console.log(bleh.getWordsWithDefinitions()[0].getWord());
+        console.log(bleh.getWordsWithDefinitions()[0].getDefinitions()[0]);
+    })
+    .catch((err: any) => { });*/
--- server/app/routes/crossword/lexiconAPI/gridWordInformation.ts
+++ server/app/routes/crossword/lexiconAPI/gridWordInformation.ts
@@ -4,19 +4,18 @@ export class GridWordInformation {
     private word: string;
     private defs: string[];
     private frequency: number;
-    private isCommon: boolean;
+    private isCommon: boolean = true;
     private wordInfo: [string, string[], number];
 
-    constructor(_word: string, _defs: string[], _frequency: number) {
-        this.word = _word;
-        this.defs = _defs;
-        this.frequency = _frequency;
-        if(this.frequency < 15) {
+    constructor(requestWord: string, requestDefs: string[], requestFrequency: number) {
+        this.word = requestWord;
+        this.defs = requestDefs;
+        this.frequency = requestFrequency;
+        const commonFactor: number = 15;
+        if (this.frequency < commonFactor) {
             this.isCommon = false;
-        }else {
-            this.isCommon = true;
         }
-        this.wordInfo = [_word, _defs, _frequency]
+        this.wordInfo = [word, defs, frequency];
     }
 
     public getWord(): string {
@@ -31,13 +30,12 @@ export class GridWordInformation {
         return this.frequency;
     }
 
-    public getIsCommon() : boolean {
+    public getIsCommon(): boolean {
         return this.isCommon;
     }
 
     public getInfo(): [string, string[], number] {
         return this.wordInfo;
     }
-
 
 }
