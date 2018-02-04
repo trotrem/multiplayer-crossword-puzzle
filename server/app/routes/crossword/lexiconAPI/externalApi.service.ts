935f5800411cf877aa26e1e5563e9ab3759eb866
Marc-Antoine De Serres <madeserres@videotron.ca>
2018-02-03 19:29:37 -0500

    fonctions qui retourne un array de gridWordInfo

--- server/app/routes/crossword/lexiconAPI/externalApi.service.ts
+++ server/app/routes/crossword/lexiconAPI/externalApi.service.ts
@@ -7,6 +7,10 @@ export class ExternalApiService {
     public wordsWithDefinitions: GridWordInformation[];
     public test: any;
 
+    constructor() { 
+        this.wordsWithDefinitions = new Array<GridWordInformation>();
+    }
+
     public options = {
         method: 'GET',
         uri: 'http://api.datamuse.com/words',
@@ -23,27 +27,27 @@ export class ExternalApiService {
         return requestOption(this.options)
             .then((result: JSON) => {
                 this.requestResult = result;
-                this.wordHasDefinition();
+                this.removeWordWithoutDefinitions();
             }, reject => {
                 console.log('Rejected, Error...');
             })
             .catch((err: any) => { console.log('Erreur...') });
     }
 
-    public getWordWithDefinition(): GridWordInformation[] {
+    public getWordsWithDefinitions(): GridWordInformation[] {
 
         return this.wordsWithDefinitions;
     }
 
-    private wordHasDefinition(): void {
+    private removeWordWithoutDefinitions(): void {
         for (let index in this.requestResult) {
-            let indexDef: number = 0;
             if (this.requestResult[index].hasOwnProperty('defs')) {
-               // let tempWord = new GridWordInformation(this.requestResult[index].word, this.requestResult[index].defs, 55)
+                let tempFrequency : string = this.requestResult[index].tags[0].substring(2);
+                let tempWord = new GridWordInformation(this.requestResult[index].word, this.requestResult[index].defs, parseFloat(tempFrequency))
+                //console.log(tempWord.getDefinitions()[0]);
                 //this.test = tempWord;
-                //this.wordsWithDefinitions[indexDef] = tempWord;
-                console.log('test...');
-                indexDef++;
+                this.wordsWithDefinitions.push(tempWord);
+                //console.log('test...');
             }
         }
 
@@ -53,8 +57,8 @@ export class ExternalApiService {
 let bleh = new ExternalApiService;
 bleh.requestWordInfo('t?e')
     .then(() => {
-        console.log(bleh.requestResult[0].frequency)
-        // console.log(bleh.wordsWithDefinitions[0].getWord());
+        //console.log(bleh.requestResult[0].tags[0])
+        console.log(bleh.wordsWithDefinitions[0].getFrequency());
     })
     .catch((err: any) => { console.log('allo') });
 
