43aa5151af38dde2752bde92543114cbe3228a2a
Marc-Antoine De Serres <madeserres@videotron.ca>
2018-02-05 23:46:07 -0500

    Remise sprint 1

--- server/app/routes/crossword/gridSaver/gridSaver.ts
+++ server/app/routes/crossword/gridSaver/gridSaver.ts
@@ -4,7 +4,7 @@ export class GridSaver {
     private _gridArray: Grid[];
 
     constructor() {
-        this._gridArray = new Array<Grid>;
+        this._gridArray = new Array<Grid>();
     }
     public addGrid(grid: Grid): void {
         this._gridArray.push(grid);
