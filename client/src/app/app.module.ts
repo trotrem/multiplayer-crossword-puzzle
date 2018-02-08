d261aa52fc979b230b482e782f8c817e4c782436
amal_metsahel <amal_metsahel@github.com>
2018-02-08 16:01:05 -0500

    :Amal M: creation de la page principale

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-
 import { AppComponent } from "./app.component";
 import { GameComponent } from "./game-component/game.component";
-
+import { RouterModule, Routes } from "@angular/router";
 import { RenderService } from "./render-service/render.service";
 import { BasicService } from "./basic.service";
 import { EditeurComponent } from "./editeur/editeur.component";
+const appRoutes: Routes = [
+  { path: "Editeur", component: EditeurComponent },
+];
 
 @NgModule({
     declarations: [
@@ -17,7 +19,10 @@ import { EditeurComponent } from "./editeur/editeur.component";
     ],
     imports: [
         BrowserModule,
-        HttpClientModule
+        HttpClientModule,
+        RouterModule.forRoot(
+            appRoutes
+          )
     ],
     providers: [
         RenderService,
--- client/src/app/app.routing.ts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { App.Routing.Ts } from './app.routing.ts';
-
-describe('App.Routing.Ts', () => {
-  it('should create an instance', () => {
-    expect(new App.Routing.Ts()).toBeTruthy();
-  });
-});
