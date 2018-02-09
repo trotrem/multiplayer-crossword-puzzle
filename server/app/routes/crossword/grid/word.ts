36721b73ef67a22d2dd7abdad483e01e5c64268f
Sarah Denis <sarah.denis@polymtl.ca>
2018-02-09 09:39:35 -0500

    Sarah Denis : ajout de position et direction dans word.ts

--- server/app/routes/crossword/grid/word.ts
+++ server/app/routes/crossword/grid/word.ts
@@ -1,16 +1,23 @@
-//classe temporaire qui serait remplacé par gridWordInformation.ts lors de l'ajout de mot dans la grille
+import { GridWordInformation } from "../lexiconAPI/gridWordInformation"
+enum Direction{
+    Y,
+    X
+}
 export class Word {
     private _length: number;
     private _id: number;
-    private _text: string;
+    private _text: GridWordInformation;
+    private _posX: number;
+    private _posY: number;
+    private direction : Direction;
 
-    constructor(length: number, id: number, text: string) {
+    constructor(length: number, id: number, text: GridWordInformation) {
         this._length = length;
         this._id = id;
         this._text = text;
     }
 
-    public get Word(): string {
+    public get Word(): GridWordInformation {
         return this._text;
     }
     public get Length(): number {
@@ -19,7 +26,7 @@ export class Word {
     public get Number(): number {
         return this._id;
     }
-    public setWord(word: string): void {
+    public setWord(word: GridWordInformation): void {
         this._text = word;
     }
 }
