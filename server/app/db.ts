74786b1e13bf8250bf2029f65a75ed5bfdaabea8
amal_metsahel <amal_metsahel@github.com>
2018-02-13 10:19:54 -0500

    Amal M:Cleaning de bd.ts

--- server/app/db.ts
+++ server/app/db.ts
@@ -1,14 +1,13 @@
-let mongoose = require("mongoose");
-let Three = require("three");
-
-
-let tracksSchema = new mongoose.Schema({
-    name : {type :String, required : true, unique : true},
+import * as mongoose from "mongoose";
+import * as THREE from "three";
+const tracksSchema: mongoose.Schema = new mongoose.Schema({
+    name : {type : String, required : true, unique : true},
     description : {type : String, default : "This is a track"},
-    startingZone: Three.Vector3,
-    points: Three.Vector3
+    startingZone: THREE.Line3,
+    points: Array(THREE.Vector3)
 });
 
-export let tracks = mongoose.model("Tracks", tracksSchema);
-mongoose.Promise= global.Promise;
-mongoose.connect("mongodb://read_bool:projet22018@ds035290.mlab.com:35290/projet2_07").catch((err:any) => {console.log(err)})
+export let tracks: mongoose.Model<mongoose.Document> = mongoose.model("Tracks", tracksSchema);
+require("mongoose").Promise = global.Promise ;
+mongoose.connect("mongodb://read_bool:projet22018@ds035290.mlab.com:35290/projet2_07").catch(
+    (err: Error) => console.error(err));
