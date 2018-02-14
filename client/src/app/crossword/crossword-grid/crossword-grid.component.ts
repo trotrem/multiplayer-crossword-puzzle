13132d6eabe309ed57dcd44a299f827dad95005d
Jean-Christophe Buteau <jeanchristophe.buteau@gmail.com>
2018-02-14 10:52:25 -0500

    JC BUTEAU - TSlint

--- client/src/app/crossword/crossword-grid/crossword-grid.component.ts
+++ client/src/app/crossword/crossword-grid/crossword-grid.component.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+const GRID_LENGTH: number = 100;
 
 @Component({
-  selector: 'app-crossword-grid',
-  templateUrl: './crossword-grid.component.html',
-  styleUrls: ['./crossword-grid.component.css']
+  selector: "app-crossword-grid",
+  templateUrl: "./crossword-grid.component.html",
+  styleUrls: ["./crossword-grid.component.css"]
 })
 export class CrosswordGridComponent implements OnInit {
-
   public cells: string[];
 
-  constructor() { 
+  public constructor() {
     this.cells = new Array<string>();
-    for(let i = 0;i<100;i++){
+    for (let i: number = 0; i < GRID_LENGTH; i++) {
       this.cells.push(i.toString());
     }
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
   }
-
 }
