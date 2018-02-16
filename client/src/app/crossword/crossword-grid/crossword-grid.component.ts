af037e0531aa6d6172adc71e3da3c4dfa5ae97f5
Jean-Christophe Buteau <jeanchristophe.buteau@gmail.com>
2018-02-16 09:58:38 -0500

    Jc Buteau - created wordDescription interface

--- client/src/app/crossword/crossword-grid/crossword-grid.component.ts
+++ client/src/app/crossword/crossword-grid/crossword-grid.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 const GRID_WIDTH: number = 10;
 const GRID_HEIGHT: number = 10;
+
+interface WordDescription {
+  direction: string;
+  x: number;
+  y: number;
+  length: number;
+  definition: string;
+}
 
 @Component({
   selector: "app-crossword-grid",
@@ -10,6 +18,16 @@ const GRID_HEIGHT: number = 10;
 })
 export class CrosswordGridComponent implements OnInit {
   public cells: string[][];
+  @Input() public nbPlayers: number;
+  private words: WordDescription[];
+
+  public get horizontalWords(): WordDescription[] {
+    return this.words.filter((word) => word.direction === "h");
+  }
+
+  public get verticalWords(): WordDescription[] {
+    return this.words.filter((word) => word.direction === "v");
+  }
 
   public constructor() {
     this.cells = new Array<Array<string>>();
@@ -20,6 +38,10 @@ export class CrosswordGridComponent implements OnInit {
       }
     }
     this.cells[3][4] = "-";
+    this.words = new Array<WordDescription>();
+    this.words.push({direction: "h", x: 2, y: 0, length: 4, definition: "word1"});
+    this.words.push({direction: "v", x: 4, y: 2, length: 6, definition: "word2"});
+    this.words.push({direction: "v", x: 2, y: 0, length: 4, definition: "word3"});
   }
 
   public ngOnInit(): void {
