c51a869bf662dc6ca8971a2f1b5a7035ecbdcdd8
amal_metsahel <amal_metsahel@github.com>
2018-02-16 09:15:36 -0500

    Amal M:cleaning routes.ts

--- server/app/routes.ts
+++ server/app/routes.ts
@@ -5,7 +5,7 @@ import Types from "./types";
 import { Index } from "./routes/index";
 import { tracks } from "./db";
 import { Document } from "mongoose";
-
+const BAD_REQUEST_ERROR: number = 400;
 @injectable()
 export class Routes {
     public constructor(@inject(Types.Index) private index: Index) {
@@ -15,20 +15,17 @@ export class Routes {
     public get routes(): Router {
         const router: Router = Router();
 
-        router.get("/",
-                    (req: Request, res: Response, next: NextFunction) => this.index.helloWorld(req, res, next));
+        router.get("/", (req: Request, res: Response, next: NextFunction) => this.index.helloWorld(req, res, next));
         router.post("/track", (req: Request, res: Response, next: NextFunction) => {
-            const myData = new tracks(req.body);
+            const myData: Document = new tracks(req.body);
             // console.log(req.body);
 
             myData.save()
                 .then((item: Document) => {
                     res.send("Name saved to database");
-                     console.log("Name saved to database");
                 })
-                .catch((err) => {
-                    res.status(400).send("Unable to save to database");
-                     console.log("Unable to save to database");
+                .catch((err: Error) => {
+                    res.status(BAD_REQUEST_ERROR).send("Unable to save to database");
                 });
         });
 
