a7a5a93526ba46628a64445a7e4841ee63843c10
amal_metsahel <amal_metsahel@github.com>
2018-02-23 14:02:50 -0500

    Amal M: l'ajout de l'attribut usesNum dans bd.ts

--- server/app/db.ts
+++ server/app/db.ts
@@ -4,7 +4,8 @@ const tracksSchema: mongoose.Schema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
     description: { type: String, default: "This is a track" },
     startingZone: mongoose.Schema.Types.Mixed,
-    points: Array(mongoose.Schema.Types.Mixed)
+    points: Array(mongoose.Schema.Types.Mixed),
+    usesNumber: {type: Number, default: 0}
 });
 
 export let tracks: mongoose.Model<mongoose.Document> = mongoose.model("Tracks", tracksSchema);
