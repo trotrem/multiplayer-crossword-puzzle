0eba363a605a4ce6f09b34debf326d2ea28262de
amal_metsahel <amal_metsahel@github.com>
2018-02-26 17:05:11 -0500

    Amal M: les test de trackCreator

--- client/src/app/racing/trackcreator/track-creator.spec.ts
+++ client/src/app/racing/trackcreator/track-creator.spec.ts
@@ -1,8 +1,41 @@
 import { TrackCreator } from "./track-creator";
-import {ActivatedRoute} from "@angular/router";
+import * as THREE from "three";
 
 describe("TrackCreator", () => {
+  const component: TrackCreator = new TrackCreator();
   it("should create an instance", () => {
-    expect(new TrackCreator()).toBeTruthy();
+    expect(component).toBeDefined();
   });
+
+  it("should create a point", () => {
+      // tslint:disable-next-line:no-magic-numbers
+      const position: THREE.Vector3 = new THREE.Vector3(-23, -2, 0);
+      const point: THREE.Points = component.createPoint(position);
+      expect(point).toBeDefined();
+  });
+
+  it("should create a line", () => {
+      const points: Array<THREE.Vector3> = new Array<THREE.Vector3>();
+      // tslint:disable-next-line:no-magic-numbers
+      const position1: THREE.Vector3 = new THREE.Vector3(-23, -2, 0);
+      // tslint:disable-next-line:no-magic-numbers
+      const position2: THREE.Vector3 = new THREE.Vector3(23, -2, 0);
+      points.push(position1);
+      component.points = points;
+      const lines: Array<THREE.Line> = component.createLine(position1, position2);
+      expect(lines.length).toBe(1);
+  });
+  it("should recreate a conflict line ", () => {
+    const illegalPoints: Array<THREE.Vector3> = new Array<THREE.Vector3>();
+    // tslint:disable-next-line:no-magic-numbers
+    const position1: THREE.Vector3 = new THREE.Vector3(-23, -2, 0);
+    // tslint:disable-next-line:no-magic-numbers
+    const position2: THREE.Vector3 = new THREE.Vector3(23, -2, 0);
+    illegalPoints.push(position1);
+    illegalPoints.push(new THREE.Vector3(0, 0, 0));
+    illegalPoints.push(position2);
+    const lines: Array<THREE.Line> = component.redrawConflictingLines(illegalPoints);
+    // tslint:disable-next-line:no-magic-numbers
+    expect(lines.length).toBe(2);
+});
 });
