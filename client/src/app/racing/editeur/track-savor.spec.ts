f7c282d0d30270ae23922c2877f966178a353055
amal_metsahel <amal_metsahel@github.com>
2018-02-26 16:32:19 -0500

    Amal M: les tests de trackSavor

--- client/src/app/racing/editeur/track-savor.spec.ts
+++ client/src/app/racing/editeur/track-savor.spec.ts
@@ -1,7 +1,24 @@
 import { TrackSavor } from "./track-savor";
 
+import { TestBed, inject } from "@angular/core/testing";
+
+import { TrackServices } from "./../track.services/track.service";
+import { HttpClientModule, HttpClient  } from "@angular/common/http";
+
 describe("TrackSavor", () => {
-  it("should create an instance", () => {
-    // expect(TrackSavor.toBeTruthy();
+  // tslint:disable-next-line:prefer-const
+  let http: HttpClient;
+  const trackSavor: TrackSavor = new TrackSavor(http);
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      providers: [TrackServices]
+    });
   });
+
+  it("should be created", inject([TrackServices], (service: TrackServices) => {
+    expect(trackSavor).toBeTruthy();
+  }));
 });
