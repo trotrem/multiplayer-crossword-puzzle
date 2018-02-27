05551d707a2a31f518d94cd7c7e8e30511a0f1bf
amal_metsahel <amal_metsahel@github.com>
2018-02-26 20:31:41 -0500

    Amal M et Laurence : les tests du service communcationService

--- client/src/app/crossword/communication.service.spec.ts
+++ client/src/app/crossword/communication.service.spec.ts
@@ -1,15 +1,62 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from "@angular/core/testing";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { Difficulty, GridData, WordValidationParameters } from "../../../../common/communication/message";
 
-import { CommunicationService } from './communication.service';
+import { CommunicationService } from "./communication.service";
 
-describe('CommunicationService', () => {
+describe("CommunicationService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CommunicationService]
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        CommunicationService]
     });
   });
 
-  it('should be created', inject([CommunicationService], (service: CommunicationService) => {
+  it("should be created", inject([CommunicationService], (service: CommunicationService) => {
     expect(service).toBeTruthy();
   }));
+
+  it("should issue a GET request when difficulty selected", async(
+    inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+      const _difficulty: Difficulty = "easy";
+      http.get("http://localhost:3000/crossword/grid/" + _difficulty.valueOf()).subscribe() ;
+      backend.expectOne({
+        url: "http://localhost:3000/crossword/grid/" + _difficulty.valueOf(),
+        method: "GET"
+      });
+    })
+  )
+  );
+
+  it("should issue a GET request when id selected", async(
+    inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+      const id: number = 1;
+      http.get("http://localhost:3000/crossword/grid/" + id).subscribe() ;
+      backend.expectOne({
+        url: "http://localhost:3000/crossword/grid/" + id,
+        method: "GET"
+      });
+    })
+  )
+  );
+  it("should issue a post request", async(
+    inject([HttpClient, HttpTestingController], (http: HttpClient, backend: HttpTestingController) => {
+      const parameters: WordValidationParameters = {
+        gridId: 1 ,
+        word: "word",
+        wordIndex: 2
+    };
+      http.post("http://localhost:3000/crossword/validate", JSON.stringify(parameters)).subscribe();
+      backend.expectOne({
+        url: "http://localhost:3000/crossword/validate",
+        method: "POST"
+      });
+    })
+  )
+  );
 });
