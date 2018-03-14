59ceb88883f8848af5530fd396163e08f8f5fd6b
amal_metsahel <amal_metsahel@github.com>
2018-03-13 21:03:25 -0400

    Amal M: l'ajout de la methode qui renvoie une position d' une paire de voitures

--- client/src/app/racing/randomCarCreator.service/random-car-creator.service.ts
+++ client/src/app/racing/randomCarCreator.service/random-car-creator.service.ts
@@ -1,15 +1,33 @@
 import { Injectable } from "@angular/core";
 import * as THREE from "three";
+import { Car } from "../car/car";
+
+const DISTANCE_BETWEEN_CARS: number = 0.5;
 @Injectable()
 export class RandomCarCreatorService {
-
-  public constructor() { }
+  // public constructor() { }
   public getRandomCarPosition(firstLine: THREE.Line3): THREE.Vector3 {
-    const randomXPosition: number = Math.floor(Math.random() * (firstLine.start.x + 1 - firstLine.end.x)) + firstLine.end.x;
-    const randomYPosition: number = Math.floor(Math.random() * (firstLine.start.y + 1 - firstLine.end.y)) + firstLine.end.y;
-    const randomZPosition: number = Math.floor(Math.random() * (firstLine.start.z + 1 - firstLine.end.z)) + firstLine.end.z;
+    const randomXPosition: number = this.getRandomNumber(firstLine.start.x, firstLine.end.x);
+    const randomYPosition: number = this.getRandomNumber(firstLine.start.y, firstLine.end.y);
+    const randomZPosition: number = this.getRandomNumber(firstLine.start.z, firstLine.end.z);
 
     return new THREE.Vector3(randomXPosition, randomYPosition, randomZPosition);
+  }
+  private getRandomNumber(start: number, end: number): number {
+
+    return Math.floor(Math.random() * (start + 1 - end)) + end;
+  }
+  private getAdjacentCarPosition(vector: THREE.Vector3): THREE.Vector3 {
+
+    return new THREE.Vector3(vector.x, vector.y + DISTANCE_BETWEEN_CARS, vector.z + DISTANCE_BETWEEN_CARS );
+  }
+  public getRandomPairOfCars(firstLine: THREE.Line3): THREE.Vector3[] {
+   const pairPositions: THREE.Vector3[] = new Array<THREE.Vector3>();
+   const firstCarPosition: THREE.Vector3 = this.getRandomCarPosition(firstLine);
+   pairPositions.push(firstCarPosition);
+   pairPositions.push(this.getAdjacentCarPosition(firstCarPosition));
+
+   return pairPositions;
   }
 
 }
