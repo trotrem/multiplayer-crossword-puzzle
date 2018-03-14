9215b71e9733a667a85de6a871fb7ae461e82837
amal_metsahel <amal_metsahel@github.com>
2018-03-13 23:28:12 -0400

    Amal M: on est capable de voir une voiture sur la zone de dèpart

--- client/src/app/racing/printCar.service/print-cars.service.ts
+++ client/src/app/racing/printCar.service/print-cars.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { RandomCarsFirstPositionsService } from "../randomCarsFirstPositions.service/random-cars-first-positions.service";
+import { Car } from "../car/car";
+import * as THREE from "three";
+
+const CARS_PAIRS_MAX: number = 2;
+const WHITE: number = 0xFFFFFF;
+const AMBIENT_LIGHT_OPACITY: number = 0.5;
 
 @Injectable()
 export class PrintCarsService {
+  private randomPositions: RandomCarsFirstPositionsService;
+  private car: Car;
+  public constructor() {
+    this.car = new Car();
+    this.randomPositions = new RandomCarsFirstPositionsService();
+  }
+  public initiateCar(camera: THREE.PerspectiveCamera, scene: THREE.Scene): Car {
+    this.car.init();
+    camera.lookAt(this.car.position);
+    scene.add(this.car);
+    scene.add(new THREE.AmbientLight(WHITE, AMBIENT_LIGHT_OPACITY));
 
-  constructor() { }
-
+    return this.car;
+}
+  public insertCars(line: THREE.Line3, scene: THREE.Scene ): void {
+    const positions: THREE.Vector3[] = this.randomPositions.getRandomPairOfAdjacentPositions(line);
+    this.translateCarPosition(positions[0]);
+    scene.add(this.car);
+    console.warn(this.car.position);
+}
+  private translateCarPosition( vector: THREE.Vector3): void {
+    this.car.translateX(vector.x);
+    this.car.translateY(vector.y);
+    this.car.translateZ(vector.z);
+  }
 }
