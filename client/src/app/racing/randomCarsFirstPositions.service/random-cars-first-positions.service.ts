b627dc7df9557538e73da9ef2e2c085e65b386eb
amal_metsahel <amal_metsahel@github.com>
2018-03-16 10:43:48 -0400

    Amal M: la position des 2 premières voitures est bonne

--- client/src/app/racing/randomCarsFirstPositions.service/random-cars-first-positions.service.ts
+++ client/src/app/racing/randomCarsFirstPositions.service/random-cars-first-positions.service.ts
@@ -6,26 +6,44 @@ const DISTANCE_BETWEEN_CARS: number = 0.01;
 @Injectable()
 export class RandomCarsFirstPositionsService {
   // public constructor() { }
-  public getRandomPosition(firstLine: THREE.Line3): THREE.Vector3 {
-    const randomXPosition: number = this.getRandomNumber(firstLine.start.x, firstLine.end.x);
-    const randomYPosition: number = this.getRandomNumber(firstLine.start.y, firstLine.end.y);
-    const randomZPosition: number = this.getRandomNumber(firstLine.start.z, firstLine.end.z);
+  public getRandomPosition(startingPoint: THREE.Vector3): THREE.Vector3 {
+    /*const randomXPosition: number = this.getRandomNumber(startingPoint.x, startingPoint.x);
+    const randomYPosition: number = this.getRandomNumber(startingPoint.y, startingPoint.y);
+    const randomZPosition: number = this.getRandomNumber(startingPoint.z, startingPoint.z);*/
+    const randomXPosition: number =  startingPoint.x;
+    const randomYPosition: number = startingPoint.y;
+    const randomZPosition: number = startingPoint.z;
 
     return new THREE.Vector3(randomXPosition, randomYPosition, randomZPosition);
   }
+  public getStartingPosition(line: THREE.Line3): THREE.Vector3 {
+    return new THREE.Vector3(((line.end.x + line.start.x) / 2), ((line.end.y + line.start.y) / 2), ((line.end.z + line.start.z)) / 2 );
+    //  return line.getCenter();
+  }
+
   private getRandomNumber(start: number, end: number): number {
 
     return Math.floor(Math.random() * (start + 1 - end)) + end;
   }
-  private getAdjacentCarPosition(vector: THREE.Vector3): THREE.Vector3 {
-
-    return new THREE.Vector3(vector.x, vector.y + DISTANCE_BETWEEN_CARS, vector.z + DISTANCE_BETWEEN_CARS );
+  private getAdjacentCarPosition(vector: THREE.Vector3, targetVector: THREE.Vector3): THREE.Vector3 {
+    const vec: THREE.Vector3 = vector.clone();
+    // vec.applyAxisAngle(targetVector.normalize(), Math.PI);
+    vec.reflect(targetVector.normalize());
+    // return vector.reflect(targetVector.normalize());
+    // return vector.applyAxisAngle(targetVector.normalize(), Math.PI);
+    return vec;
   }
+  private getDeltaVector(line: THREE.Line3): THREE.Vector3 {
+    return new THREE.Vector3((line.end.x - line.start.x), (line.end.y - line.start.y), (line.end.z - line.start.z));
+    }
   public getRandomPairOfAdjacentPositions(firstLine: THREE.Line3): THREE.Vector3[] {
    const pairPositions: THREE.Vector3[] = new Array<THREE.Vector3>();
-   const firstCarPosition: THREE.Vector3 = this.getRandomPosition(firstLine);
+   const targetVector: THREE.Vector3 = this.getDeltaVector(firstLine);
+   const firstCarPosition: THREE.Vector3 = this.getRandomPosition(this.getStartingPosition(firstLine));
+   // firstLine.delta(targetVector);
    pairPositions.push(firstCarPosition);
-   pairPositions.push(this.getAdjacentCarPosition(firstCarPosition));
+   pairPositions.push(this.getAdjacentCarPosition(firstCarPosition, targetVector));
+  //  pairPositions.push(firstCarPosition);
 
    return pairPositions;
   }
