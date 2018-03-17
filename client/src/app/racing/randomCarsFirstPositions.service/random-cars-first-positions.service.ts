81273e20f6a4df0adb69576ef172c65061053d99
amal_metsahel <amal_metsahel@github.com>
2018-03-16 22:38:09 -0400

    Amal M : L' ajout des positions des deux autres voitures

--- client/src/app/racing/randomCarsFirstPositions.service/random-cars-first-positions.service.ts
+++ client/src/app/racing/randomCarsFirstPositions.service/random-cars-first-positions.service.ts
@@ -3,15 +3,16 @@ import * as THREE from "three";
 
 const FIRST_CAR_INDEX: number = 2;
 const HALF_LINE_DEVIDER: number = 2 ;
-const QUARTER_LINE_DEVIDER: number = 4 ;
 const SECOND_CAR_INDEX: number = 1;
-const FIRST_CARS_X_POSITION: number = 5;
 @Injectable()
 export class RandomCarsFirstPositionsService {
-  public getStartingPosition(line: THREE.Line3, index: number): THREE.Vector3 {
-
+  private getStartingPosition(line: THREE.Line3, index: number): THREE.Vector3 {
     return new THREE.Vector3(((line.end.x + line.start.x) / index),
                              ((line.end.y + line.start.y) / index), ((line.end.z + line.start.z)) / index );
+  }
+  private getMaxDistance(line: THREE.Line3): number {
+    return Math.sqrt(Math.pow((line.end.x - line.start.x), HALF_LINE_DEVIDER) +
+     Math.pow((line.end.y - line.start.y), HALF_LINE_DEVIDER) + Math.pow((line.end.y - line.start.y), HALF_LINE_DEVIDER));
   }
 
   private getCarPosition(vector: THREE.Vector3, index: number): THREE.Vector3 {
@@ -27,8 +28,14 @@ export class RandomCarsFirstPositionsService {
   public getRandomPairOfAdjacentPositions(firstLine: THREE.Line3): THREE.Vector3[] {
    const pairPositions: THREE.Vector3[] = new Array<THREE.Vector3>();
    const firstCarPosition: THREE.Vector3 = this.getStartingPosition(firstLine, HALF_LINE_DEVIDER).clone();
-   pairPositions.push(this.getCarPosition(firstCarPosition, FIRST_CAR_INDEX).setX(FIRST_CARS_X_POSITION));
-   pairPositions.push(this.getCarPosition(firstCarPosition, SECOND_CAR_INDEX).setX(FIRST_CARS_X_POSITION));
+   pairPositions.push(this.getCarPosition(firstCarPosition, FIRST_CAR_INDEX));
+   pairPositions.push(this.getCarPosition(firstCarPosition, SECOND_CAR_INDEX));
+
+   pairPositions.push(this.getCarPosition(firstCarPosition, FIRST_CAR_INDEX).
+   setX(this.getCarPosition(firstCarPosition, SECOND_CAR_INDEX).x + this.getMaxDistance(firstLine) / 4));
+
+   pairPositions.push(this.getCarPosition(firstCarPosition, SECOND_CAR_INDEX).
+   setX(this.getCarPosition(firstCarPosition, SECOND_CAR_INDEX).x + this.getMaxDistance(firstLine) / 4));
 
    return pairPositions;
   }
