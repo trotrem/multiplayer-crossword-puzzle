e81337614469284738e78081fb0c2e96035f95aa
Laurence Duperron-Colbert <laurence.duperron-colbert@polymtl.ca>
2018-03-19 15:56:56 -0400

    Laurence Dc: test dans homePage crossword -> should create

--- client/src/app/crossword/home-page/home-page.component.spec.ts
+++ client/src/app/crossword/home-page/home-page.component.spec.ts
@@ -1,23 +1,29 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed, inject } from "@angular/core/testing";
 
 import { HomePageComponent } from "./home-page.component";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe("HomePageComponent", () => {
-  let component: HomePageComponent;
+  let router: Router;
+  let component: HomePageComponent = new HomePageComponent(router);
   let fixture: ComponentFixture<HomePageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePageComponent ]
+      declarations: [HomePageComponent],
+      imports: [FormsModule, RouterTestingModule.withRoutes([])]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(inject([Router], (_router: Router) => {
+    router = _router;
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it("should create", () => {
     expect(component).toBeTruthy();
