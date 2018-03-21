91849ac42333ced1fb563c8a645991cc22823a23
Sarah Denis <sarah.denis@polymtl.ca>
2018-03-21 17:14:23 -0400

    Sarah : commit avant merge

--- client/src/app/racing/camera/topView-camera.ts
+++ client/src/app/racing/camera/topView-camera.ts
@@ -1,13 +1,13 @@
 import * as THREE from "three";
 
 export class OrthographicCamera extends THREE.OrthographicCamera {
-    private static DISTANCE_TO_TARGET: number = 100;
+    private static DISTANCE_TO_TARGET: number = 1000;
     private static readonly WIDTH: number = window.innerWidth;
     private static readonly HEIGHT: number = window.innerHeight;
-    private static readonly ORTHO_HEIGHT: number = 25;
+    private static readonly ORTHO_HEIGHT: number = 1000;
     private static readonly ASPECT: number = OrthographicCamera.WIDTH / OrthographicCamera.HEIGHT;
-    private static readonly NEAR: number = -OrthographicCamera.DISTANCE_TO_TARGET;
-    private static readonly FAR: number = 500;
+    private static readonly NEAR: number = 1;
+    private static readonly FAR: number = 1000;
 
     private target: THREE.Object3D;
 
@@ -20,20 +20,28 @@ export class OrthographicCamera extends THREE.OrthographicCamera {
             OrthographicCamera.NEAR,
             OrthographicCamera.FAR
         );
-       // this.setupOrthographicView();
+        //this.setupOrthographicView();
     }
 
-    public setTarget(object: THREE.Object3D): void{
+    public setTarget(object: THREE.Object3D): void {
         this.target = object;
     }
 
-    public updatePosition(): void {
-        this.position.copy(this.target.position).add(new THREE.Vector3(0, OrthographicCamera.DISTANCE_TO_TARGET, 0));
+    public setStartPosition(position: THREE.Vector3, carPosition: THREE.Vector3): void{
+        this.position.copy(position);
+        this.lookAt(carPosition);
+    }
+
+    public updatePosition(position: THREE.Vector3): void {
+        this.position.copy(position).add(new THREE.Vector3(0, 0, OrthographicCamera.DISTANCE_TO_TARGET));
+        this.updateProjectionMatrix();
+
     }
 
     private setupOrthographicView(): void {
-        this.rotation.order = "YXZ";
-        this.rotateX(-Math.PI / 2);
+        //this.rotation.order = "YXZ";
+        this.rotateX(Math.PI / 2);
+        this.updateProjectionMatrix();
     }
 
 }
