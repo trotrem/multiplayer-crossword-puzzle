8ce1fdf445c04afbb1aa54ba75029389d73060d4
Laurence Duperron-Colbert <laurence.duperron-colbert@polymtl.ca>
2018-03-22 16:50:48 -0400

    Laurence DC: ajout d;un test dans user component pour tester la navigation

--- client/src/app/racing/user/user.component.spec.ts
+++ client/src/app/racing/user/user.component.spec.ts
@@ -8,18 +8,20 @@ import { Track } from "../track";
 import { CommunicationRacingService } from "../communication.service/communicationRacing.service";
 
 import * as THREE from "three";
+import { GameComponent } from "../game-component/game.component";
 
 describe("UserComponent", () => {
     let component: UserComponent;
     let fixture: ComponentFixture<UserComponent>;
+    let router: Router;
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [UserComponent],
+            declarations: [UserComponent, GameComponent],
             imports: [
                 HttpClientModule,
                 HttpClientTestingModule,
-                RouterTestingModule
-            ],
+                RouterTestingModule.withRoutes([{ path: "race/:name", component: GameComponent }])]
+            ,
             providers: [CommunicationRacingService]
         })
             .compileComponents();
@@ -27,11 +29,12 @@ describe("UserComponent", () => {
 
     }));
 
-    beforeEach(() => {
+    beforeEach(inject([Router], (_router: Router) => {
+        router = _router;
         fixture = TestBed.createComponent(UserComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-    });
+    }));
 
     it("should create user component", () => {
         expect(component).toBeTruthy();
@@ -39,6 +42,14 @@ describe("UserComponent", () => {
 
     it("should containts a tracks's list", () => {
         expect(component.getTracksList()).toBeDefined();
-      });
+    });
+
+    it('navigate to "race/name" takes you to  "race/name"', fakeAsync(() => {
+        const name: string = "Laurence";
+        router.navigateByUrl("/race/" + name);
+        /* tslint:disable */
+        tick(50);
+        expect(router.url).toBe("/race/" + name);
+    }));
 
 });
