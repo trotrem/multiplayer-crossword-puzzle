5626520288c6acba8d6ea628b4b644be68ac5a8e
amal_metsahel <amal_metsahel@github.com>
2018-03-23 22:53:42 -0400

    Amal M: fixation des tests de renderService

--- client/src/app/racing/render-service/render.service.spec.ts
+++ client/src/app/racing/render-service/render.service.spec.ts
@@ -1,19 +1,32 @@
-import { TestBed, inject } from "@angular/core/testing";
+import { TestBed, inject, async } from "@angular/core/testing";
 import { Car } from "../car/car";
 import * as THREE from "three";
 import { RenderService } from "./render.service";
 import { CarsPositionsHandler } from "../cars-positions-handler/cars-positions-handler";
+import { Routes, Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { GameResultsComponent } from "../game-results/game-results.component";
 // "magic numbers" utilisés pour les tests
 /* tslint:disable:no-magic-numbers */
 describe("RenderService", () => {
     // tslint:disable-next-line:prefer-const
     let container: HTMLDivElement;
     const points: THREE.Vector3[] = new Array<THREE.Vector3>();
-    beforeEach(() => {
+    let router: Router;
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
-            providers: [RenderService, CarsPositionsHandler]
+            declarations: [GameResultsComponent],
+            imports: [
+                RouterTestingModule.withRoutes([{ path: "race/:name", component: GameResultsComponent }])]
+            ,
+            providers: [RenderService]
         });
-    });
+
+    }));
+
+    beforeEach(inject([Router], (_router: Router) => {
+        router = _router;
+    }));
 
     it("should be created", inject([RenderService], (service: RenderService) => {
         expect(service).toBeTruthy();
