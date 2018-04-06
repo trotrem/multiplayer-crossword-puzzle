fd54876598927950b3b1e62b36e41401ecc5e684
Sarah Denis <sarah.denis@polymtl.ca>
2018-04-06 13:45:23 -0400

    Sarah : Review de AI

--- client/src/app/racing/components/racing-game/ai-controller/ai-controller.ts
+++ client/src/app/racing/components/racing-game/ai-controller/ai-controller.ts
@@ -14,51 +14,58 @@ export class AiController {
 
     public isStarted: boolean = false;
     private _checkPoints: THREE.Vector3[] = new Array<THREE.Vector3>();
-    private hadTurned: boolean = false;
-    private collision: boolean = false;
-    private maxDistance: number;
+    private hasTurned: boolean = false;
+    private hasCollided: boolean = false;
+    private distanceToCorner: number;
 
+    // TODO : Inject collisionWallService (later)
     public constructor(private _car: Car, points: THREE.Vector3[], private collisionWallService: WallsCollisionsService) {
         this._checkPoints.shift();
         this._checkPoints = points.slice().reverse();
-        this.maxDistance = this.randomIntFromInterval(MIN_DISTANCE, MAX_DISTANCE);
+        this.distanceToCorner = this.randomIntFromInterval(MIN_DISTANCE, MAX_DISTANCE);
     }
 
     public update(): void {
-        // TODO : Extraire dans une methode updateSpeed (méthode moins de 10 lignes)
-        if (this._car.speed.length() < this.randomIntFromInterval(MIN_SPEED, MAX_SPEED)) {
-            this.moveForward();
-        } else {
-            this.brake();
-        }
-        // TODO : Extraire le if dans une autre méthode (méthode moins de 10 lignes)
-        if (this._car.checkpoint === this._checkPoints.length - 1) {
-            if (RaceUtils.calculateDistance(this._car.mesh.position, this._checkPoints[this._car.checkpoint]) < this.maxDistance) {
-                this._car.checkpoint = 0;
-            }
-        }
+        this.updateSpeed();
+        this.updateCheckPoint();
         this.verifyCollisionWall();
         this.turnCorner();
     }
 
+    private updateSpeed(): void {
+        if (this._car.speed.length() < this.randomIntFromInterval(MIN_SPEED, MAX_SPEED)) {
+            this.accelerate();
+        } else {
+            this.brake();
+        }
+    }
+
+    private updateCheckPoint(): void {
+        if (this._car.checkpoint === this._checkPoints.length - 1) {
+            if (RaceUtils.calculateDistance(this._car.mesh.position, this._checkPoints[this._car.checkpoint]) < this.distanceToCorner) {
+                this._car.checkpoint = 0;
+            }
+        }
+    }
+
     private turnCorner(): void {
-        if (RaceUtils.calculateDistance(this._car.mesh.position, this._checkPoints[this._car.checkpoint]) < this.maxDistance) {
+        if (RaceUtils.calculateDistance(this._car.mesh.position, this._checkPoints[this._car.checkpoint]) < this.distanceToCorner) {
             this.turn(this._car.checkpoint + 1);
-            this.hadTurned = true;
-        } else if (this.hadTurned) {
+            this.hasTurned = true;
+        } else if (this.hasTurned) {
             this._car.releaseSteering();
             this._car.checkpoint++;
-            this.hadTurned = false;
+            this.hasTurned = false;
         }
     }
 
     private verifyCollisionWall(): void {
         if (this.collisionWallService.getCollisionNormal(this._car).length > 0) {
             this.turn(this._car.checkpoint);
-            this.collision = true;
-        } else if (this.calculateAngle(this._car.checkpoint) < MAX_ANGLE && this.collision) {
+            this.hasCollided = true;
+        } else if (this.calculateAngle(this._car.checkpoint) < MAX_ANGLE && this.hasCollided) {
             this._car.releaseSteering();
-            this.collision = false;
+            this.hasCollided = false;
         }
     }
 
@@ -69,12 +76,13 @@ export class AiController {
             0)) * HALF_CIRCLE_DEGREES / Math.PI;
     }
 
+    // TODO : À vérifier, utils pour gridword et racing ? utilisé dans les 2
     private randomIntFromInterval(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     private turn(index: number): void {
-        if (this.calculeOrientation(index) > 0) {
+        if (this.calculateOrientation(index) > 0) {
             this._car.steerRight();
         } else {
             this._car.steerLeft();
@@ -82,7 +90,7 @@ export class AiController {
         this._car.isAcceleratorPressed = true;
     }
 
-    private calculeOrientation(index: number): number {
+    private calculateOrientation(index: number): number {
         return this._car.direction.cross(new Vector3(
             this._car.mesh.position.x - this._checkPoints[index].x,
             this._car.mesh.position.y - this._checkPoints[index].y,
@@ -94,7 +102,7 @@ export class AiController {
         this._car.brake();
     }
 
-    private moveForward(): void {
+    private accelerate(): void {
         this._car.releaseBrakes();
         this._car.isAcceleratorPressed = true;
     }
