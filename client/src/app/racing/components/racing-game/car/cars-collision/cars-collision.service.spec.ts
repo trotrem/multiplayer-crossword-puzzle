dd37a2d697be469bca30976e4723478e0fdd7830
Sarah Denis <sarah.denis@polymtl.ca>
2018-04-09 17:42:44 -0400

    Fix premier test collison

--- client/src/app/racing/components/racing-game/car/cars-collision/cars-collision.service.spec.ts
+++ client/src/app/racing/components/racing-game/car/cars-collision/cars-collision.service.spec.ts
@@ -4,19 +4,28 @@ import { CarsCollisionService } from "./cars-collision.service";
 import { KeyboardService } from "../../commands/keyboard.service";
 import { WallsCollisionsService } from "../../walls-collisions-service/walls-collisions-service";
 import { Car } from "../car";
+import { CarLoader } from "../car-loader";
 describe("CarsCollisionService", () => {
-    it("Should detect a collision between two cars", () => {
-        const keyboard: KeyboardService = new KeyboardService();
-        const wallsCollision: WallsCollisionsService = new WallsCollisionsService();
-        const car1: Car = new Car(wallsCollision, keyboard);
-        const car2: Car = new Car(wallsCollision, keyboard);
-        const carsCollision: CarsCollisionService = new CarsCollisionService();
-        let collision: boolean = false;
-        // les deux car devrait se superposer
-        car1.mesh.position.set(0, 0, 0);
-        car2.mesh.position.set(1, 1, 0);
-        collision = carsCollision.detectCollision(car1, car2);
-        expect(collision).toBe(true);
+    const carLoader: CarLoader = new CarLoader();
+    const wallsCollisionsService: WallsCollisionsService = new WallsCollisionsService();
+    const keyboard: KeyboardService = new KeyboardService;
+    const carsCollision: CarsCollisionService = new CarsCollisionService();
+
+    it("Should detect a collision between two cars", async () => {
+        const cars: Car[] = new Array<Car>();
+        let isCollided: boolean = false;
+        for (let i: number = 0; i < 2; i++) {
+            cars.push(new Car(wallsCollisionsService, keyboard));
+            cars[i].mesh = await carLoader.load();
+        }
+        cars[0].mesh.position.set(0, 0, 0);
+        cars[1].mesh.position.set(1, 1, 0);
+        isCollided = carsCollision.detectCollision(cars);
+        expect(isCollided).toBe(true);
+    });
+
+    it("should Handle collision", () => {
+
     });
 
 });
