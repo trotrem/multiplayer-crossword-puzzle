024f75d4bde8f6268b4eba96cef05b6866eec712
Sarah Denis <sarah.denis@polymtl.ca>
2018-04-10 09:41:52 -0400

    Sarah : Correction apres le review des textures

--- client/src/app/racing/components/racing-game/render-service/skybox.ts
+++ client/src/app/racing/components/racing-game/render-service/skybox.ts
@@ -4,37 +4,37 @@ const SKYBOX_POSITION: number = 800;
 const SKYBOX_SIZE: number = 5000;
 
 export class Skybox {
-    private skyMaterials: THREE.MeshBasicMaterial[] = [
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load("./../../assets/models/Skybox/front.JPG"),
-            side: THREE.DoubleSide
-        }),
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load("./../../assets/models/Skybox/back.JPG"),
-            side: THREE.DoubleSide
-        }),
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load("./../../assets/models/Skybox/top.JPG"),
-            side: THREE.DoubleSide
-        }),
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load("./../../assets/models/Skybox/bottom.JPG"),
-            side: THREE.DoubleSide
-        }),
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load("./../../assets/models/Skybox/right.JPG"),
-            side: THREE.DoubleSide
-        }),
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load("./../../assets/models/Skybox/left.JPG"),
-            side: THREE.DoubleSide
-        })];
-    private skyMesh: THREE.Mesh = null;
-    private skyMaterial: THREE.MeshFaceMaterial = null;
-    private skyGeometry: THREE.BoxGeometry = null;
+    private skyMaterials: THREE.MeshBasicMaterial[];
+    private skyMesh: THREE.Mesh;
+    private skyMaterial: THREE.MeshFaceMaterial;
+    private skyGeometry: THREE.BoxGeometry;
 
-    // TODO: initialiser skyMaterials dans contructeur
     public constructor() {
+        this.skyMaterials = [
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load("./../../assets/models/Skybox/front.JPG"),
+                side: THREE.DoubleSide
+            }),
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load("./../../assets/models/Skybox/back.JPG"),
+                side: THREE.DoubleSide
+            }),
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load("./../../assets/models/Skybox/top.JPG"),
+                side: THREE.DoubleSide
+            }),
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load("./../../assets/models/Skybox/bottom.JPG"),
+                side: THREE.DoubleSide
+            }),
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load("./../../assets/models/Skybox/right.JPG"),
+                side: THREE.DoubleSide
+            }),
+            new THREE.MeshBasicMaterial({
+                map: new THREE.TextureLoader().load("./../../assets/models/Skybox/left.JPG"),
+                side: THREE.DoubleSide
+            })];
         this.skyMaterial = new THREE.MeshFaceMaterial(this.skyMaterials);
         this.skyGeometry = new THREE.BoxGeometry(SKYBOX_SIZE, SKYBOX_SIZE, SKYBOX_SIZE);
         this.skyMesh = new THREE.Mesh(this.skyGeometry, this.skyMaterial);
