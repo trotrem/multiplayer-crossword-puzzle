45b77a4784ffabca1557d73211a3011090f8f4f3
Sarah Denis <sarah.denis@polymtl.ca>
2018-04-14 15:57:37 -0400

    Sarah: Refactor collisions

--- client/src/app/racing/components/racing-game/car/cars-collision/cars-collision.service.ts
+++ client/src/app/racing/components/racing-game/car/cars-collision/cars-collision.service.ts
@@ -6,14 +6,13 @@ import { Injectable } from "@angular/core";
 export interface IProjection {
     minProj: number;
     maxProj: number;
-    minDot: number;
-    maxDot: number;
 }
 
 const CAR_1_MOMENTUM_FACTOR: number = 3.9;
 const CAR_2_MOMENTUM_FACTOR: number = 4.1;
 const MINIMUM_SPEED: number = 0.05;
-const SPEED_VECTOR_FACTOR: THREE.Vector3 = new THREE.Vector3(2.5, 0, 2.5);
+const SPEED_FACTOR: number = 2.5;
+const SPEED_VECTOR_FACTOR: THREE.Vector3 = new THREE.Vector3(SPEED_FACTOR, 0, SPEED_FACTOR);
 
 @Injectable()
 export class CarsCollisionService {
@@ -31,69 +30,11 @@ export class CarsCollisionService {
     public initializeCars(cars: Car[]): void {
         this._cars = cars;
     }
-
-    private prepareShape(car: Car): THREE.Vector3[] {
-        return car.getCorners(car.getUpdatedPosition().add(car.velocity));
-    }
-
-    private shapeAroundCar(cars: Car[]): void {
-        for (let i = 0; i < cars.length; i++) {
-            this._normals[i] = cars[i].getNormals();
-            this._vecCars[i] = this.prepareShape(cars[i]);
-        }
-    }
-
-    /*********************************************************************REFACTOR*********************************************************************/
-    private detectCollision(cars: Car[]): boolean {
-        this.shapeAroundCar(cars);
-        let resultsP: IProjection[] = [];
-        let resultsQ: IProjection[] = [];
-        let resultsR: IProjection[] = [];
-        let resultsS: IProjection[] = [];
-        for (let i = 0; i < cars.length; i++) {
-            resultsP[i] = this.getMinMax(this._vecCars[i], this._normals[0][1]);
-            resultsQ[i] = this.getMinMax(this._vecCars[i], this._normals[0][0]);
-            resultsR[i] = this.getMinMax(this._vecCars[i], this._normals[1][1]);
-            resultsS[i] = this.getMinMax(this._vecCars[i], this._normals[1][0]);
-        }
-        const separateP: boolean = resultsP[0].maxProj < resultsP[1].minProj || resultsP[1].maxProj < resultsP[0].minProj;
-        const separateQ: boolean = resultsQ[0].maxProj < resultsQ[1].minProj || resultsQ[1].maxProj < resultsQ[0].minProj;
-        const separateR: boolean = resultsR[0].maxProj < resultsR[1].minProj || resultsR[1].maxProj < resultsR[0].minProj;
-        const separateS: boolean = resultsS[0].maxProj < resultsS[1].minProj || resultsS[1].maxProj < resultsS[0].minProj;
-
-        return !(separateP || separateQ || separateR || separateS);
-    }
-    /**************************************************************************************************************************************************/
-
-    /*********************************************************************REFACTOR*********************************************************************/
-    private getMinMax(vecCar: THREE.Vector3[], axis: THREE.Vector3): IProjection {
-        const points: IProjection = {
-            minProj: vecCar[1].dot(axis),
-            minDot: 1,
-            maxProj: vecCar[1].dot(axis),
-            maxDot: 1,
-        };
-        for (let j: number = 2; j < vecCar.length; j++) {
-            const currProj: number = vecCar[j].dot(axis);
-            if (points.minProj > currProj) {
-                points.minProj = currProj;
-                points.minDot = j;
-            }
-            if (currProj > points.maxProj) {
-                points.maxProj = currProj;
-                points.maxDot = j;
-            }
-        }
-
-        return points;
-    }
-    /**************************************************************************************************************************************************/
-
     public checkCarsCollisions(): void {
         let tempCars: Car[] = [];
         for (let i: number = 0; i < this._cars.length - 1; i++) {
             for (let j: number = i + 1; j < this._cars.length; j++) {
-                tempCars = [this._cars[i], this._cars[j]]
+                tempCars = [this._cars[i], this._cars[j]];
                 if (this.detectCollision(tempCars)) {
                     this.handleCollisions(tempCars);
                 }
@@ -102,31 +43,91 @@ export class CarsCollisionService {
 
     }
 
-     /*********************************************************************REFACTOR*********************************************************************/
+    private detectCollision(cars: Car[]): boolean {
+        this.shapeAroundCar(cars);
+        const resultsP: IProjection[] = [];
+        const resultsQ: IProjection[] = [];
+        const resultsR: IProjection[] = [];
+        const resultsS: IProjection[] = [];
+        for (let i: number = 0; i < cars.length; i++) {
+            resultsP[i] = this.getMinMax(this._vecCars[i], this._normals[0][1]);
+            resultsQ[i] = this.getMinMax(this._vecCars[i], this._normals[0][0]);
+            resultsR[i] = this.getMinMax(this._vecCars[i], this._normals[1][1]);
+            resultsS[i] = this.getMinMax(this._vecCars[i], this._normals[1][0]);
+        }
+        const separateP: boolean = this.result(resultsP);
+        const separateQ: boolean = this.result(resultsQ);
+        const separateR: boolean = this.result(resultsR);
+        const separateS: boolean = this.result(resultsS);
+
+        return !(separateP || separateQ || separateR || separateS);
+    }
+    private shapeAroundCar(cars: Car[]): void {
+        for (let i: number = 0; i < cars.length; i++) {
+            this._normals[i] = cars[i].getNormals();
+            this._vecCars[i] = this.prepareShape(cars[i]);
+        }
+    }
+
+    private prepareShape(car: Car): THREE.Vector3[] {
+        return car.getCorners(car.getUpdatedPosition().add(car.velocity));
+    }
+    private getMinMax(vecCar: THREE.Vector3[], axis: THREE.Vector3): IProjection {
+        const points: IProjection = {
+            minProj: vecCar[1].dot(axis),
+            maxProj: vecCar[1].dot(axis),
+        };
+        for (let j: number = 2; j < vecCar.length; j++) {
+            const currProj: number = vecCar[j].dot(axis);
+            if (points.minProj > currProj) {
+                points.minProj = currProj;
+            }
+            if (currProj > points.maxProj) {
+                points.maxProj = currProj;
+            }
+        }
+
+        return points;
+    }
+    private result(results: IProjection[]): boolean {
+        return results[0].maxProj < results[1].minProj || results[1].maxProj < results[0].minProj;
+    }
+
     private handleCollisions(cars: Car[]): void {
         const totalMass: number = cars[0].Mass + cars[1].Mass;
         const phi: number = cars[0].getUpdatedPosition().angleTo(cars[1].getUpdatedPosition()); // Angle entre les 2 positions de voitures
 
-        // Produire un tableau pour rapetisser?
+        const theta1: number = this.getTheta(cars[0]);
+        const theta2: number = this.getTheta(cars[1]);
 
-        const theta1: number = cars[0].speed.length() !== 0 ? Math.acos(cars[0].speed.x / cars[0].speed.length()) : 0; // Angle de vitesse
-        const theta2: number = cars[1].speed.length() !== 0 ? Math.acos(cars[1].speed.x / cars[1].speed.length()) : 0; // Angle de vitesse
-
-        const temp1: number = (cars[1].Mass * cars[1].speed.length() * Math.cos(theta2 - phi) * 2) / totalMass;
-        const temp2: number = cars[0].speed.length() * Math.sin(theta1 - phi);
-        const temp3: number = (cars[0].Mass * cars[0].speed.length() * Math.cos(theta1 - phi) * 2) / totalMass;
-        const temp4: number = (cars[1].speed.length() * Math.sin(theta2 - phi));
-
-        const newSpeedX1: number = ((temp1 * Math.cos(phi)) - (temp2 * Math.sin(phi))) / CAR_1_MOMENTUM_FACTOR;
-        const newSpeedZ1: number = ((temp1 * Math.sin(phi)) + (temp2 * Math.cos(phi))) / CAR_1_MOMENTUM_FACTOR;
-        const newSpeedX2: number = ((temp3 * Math.cos(phi)) - (temp4 * Math.sin(phi))) / CAR_2_MOMENTUM_FACTOR;
-        const newSpeedZ2: number = ((temp3 * Math.sin(phi)) + (temp4 * Math.cos(phi))) / CAR_2_MOMENTUM_FACTOR;
-
-        const newSpeed: THREE.Vector3[] = [new THREE.Vector3(newSpeedX1, 0, newSpeedZ1), new THREE.Vector3(newSpeedX2, 0, newSpeedZ2)]
-
-        for (let i = 0; i < cars.length; i++) {
+        const newSpeed: THREE.Vector3[] = this.getNewSpeed(cars, theta1, theta2, phi, totalMass);
+        for (let i: number = 0; i < cars.length; i++) {
             cars[i].speed = cars[i].speed.length() > MINIMUM_SPEED ? newSpeed[i] : cars[i].speed.add(SPEED_VECTOR_FACTOR);
         }
     }
-    /**************************************************************************************************************************************************/
+    private getTheta(car: Car): number {
+        return car.speed.length() !== 0 ? Math.acos(car.speed.x / car.speed.length()) : 0; // Angle de vitesse
+    }
+    private getNewSpeed(cars: Car[], theta1: number, theta2: number, phi: number, totalMass: number): THREE.Vector3[] {
+        const const1Speed1: number = this.getConst1(cars[1], theta2, phi, totalMass);
+        const const2Speed1: number = this.getConst2(cars[0], theta1, phi);
+        const const1Speed2: number = this.getConst1(cars[0], theta1, phi, totalMass);
+        const const2Speed2: number = this.getConst2(cars[1], theta2, phi);
+
+        return [
+            new THREE.Vector3(this.getSpeedX(const1Speed1, const2Speed1, phi), 0, this.getSpeedZ(const1Speed1, const2Speed1, phi)),
+            new THREE.Vector3(this.getSpeedX(const1Speed2, const2Speed2, phi), 0, this.getSpeedZ(const1Speed2, const2Speed2, phi))];
+    }
+    private getSpeedX(const1: number, const2: number, phi: number): number {
+        return ((const1 * Math.cos(phi)) - (const2 * Math.sin(phi))) / CAR_1_MOMENTUM_FACTOR;
+    }
+    private getSpeedZ(const1: number, const2: number, phi: number): number {
+        return ((const1 * Math.cos(phi)) - (const2 * Math.sin(phi))) / CAR_1_MOMENTUM_FACTOR;
+    }
+    private getConst1(car: Car, theta: number, phi: number, totalMass: number): number {
+        return (car.Mass * car.speed.length() * Math.cos(theta - phi) * 2) / totalMass;
+    }
+    private getConst2(car: Car, theta: number, phi: number): number {
+        return car.speed.length() * Math.sin(theta - phi);
+    }
 }
