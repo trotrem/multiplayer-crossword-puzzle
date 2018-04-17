0867c19aa1dc119239f93f50c33652e491b6dffd
Laurence Duperron-Colbert <laurence.duperron-colbert@polymtl.ca>
2018-04-17 14:04:07 -0400

    Laurence DC: deuxime division de grid-event dans word-status-manger

--- client/src/app/crossword/grid-event.service/grid-event.service.ts
+++ client/src/app/crossword/grid-event.service/grid-event.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from "@angular/core";
-import { Difficulty, NbPlayers, GameResult } from "../../../../../common/communication/types";
+import { NbPlayers, GameResult } from "../../../../../common/communication/types";
 import { CommunicationService } from "./../communication.service";
 import { Router } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
-import { Subscription } from "rxjs/Subscription";
-import { IValidationData, IWordSelection, IGameResult, ICrosswordSettings, IWordValidationPayload } from "../../../../../common/communication/events";
-import { WordDescription, Cell, AssociatedPlayers } from "./../dataStructures";
-import { PlayManager } from "./../Nouveau/play-manager";
+import { IValidationData, IWordSelection, IGameResult} from "../../../../../common/communication/events";
+import { WordDescription, Cell, AssociatedPlayers, SelectedWord } from "./../dataStructures";
+import { PlayManager } from "./../play-manager/play-manager";
+import { WordStatusManager } from "./../word-status-manager/word-status-manager";
 
 const BACKSPACE: number = 8;
 const DELETE: number = 46;
@@ -14,11 +13,6 @@ const UPPER_A: number = 65;
 const UPPER_Z: number = 90;
 const LOWER_A: number = 97;
 const LOWER_Z: number = 122;
-
-interface SelectedWord {
-    player: AssociatedPlayers;
-    word: WordDescription;
-}
 
 @Injectable()
 export class GridEventService {
@@ -53,44 +47,14 @@ export class GridEventService {
     private subscribeToOpponentSelection(): void {
         this.communicationService.onOpponentSelectedWord().subscribe((word: IWordSelection) => {
             console.log("received");
-            this.setSelectedWord(this._opponentSelectedWord, word.wordId !== null ? this._words[word.wordId] : null, true);
+            WordStatusManager.setSelectedWord(
+                this._opponentSelectedWord, word.wordId !== null ? this._words[word.wordId] : null, true,
+                this.communicationService, this._id, this._nbPlayers);
         });
     }
 
     public setPlayerSelectedWord(word: WordDescription, selected: boolean): WordDescription {
-        return this.setSelectedWord(this._selectedWord, word, selected);
-    }
-    // dans data-structure
-    // TODO: selected inutile
-    public setSelectedWord(target: SelectedWord, word: WordDescription, selected: boolean): WordDescription {
-        if (this._nbPlayers === 2 && target.player === AssociatedPlayers.PLAYER) {
-            console.log("sending");
-            this.communicationService.sendSelectionStatus({ gameId: this._id, wordId: word !== null ? word.id : null });
-        }
-
-        if (target.word === word) {
-            return null;
-        }
-        if (target.word !== null) {
-            this.setWordSelectedState(target, target.word, false);
-            target.word = null;
-        }
-        if (word !== null && selected) {
-            this.setWordSelectedState(target, word, true);
-            target.word = word;
-        }
-
-        return target.word;
-    }
-    // dans data-structure
-    private setWordSelectedState(target: SelectedWord, word: WordDescription, selected: boolean): void {
-        for (const cell of word.cells) {
-            if (selected) {
-                cell.selectedBy = cell.selectedBy | target.player;
-            } else {
-                cell.selectedBy = cell.selectedBy & ~target.player;
-            }
-        }
+        return WordStatusManager.setSelectedWord(this._selectedWord, word, selected, this.communicationService, this._id, this._nbPlayers);
     }
 
     public onCellClicked(event: MouseEvent, cell: Cell): WordDescription {
@@ -101,13 +65,15 @@ export class GridEventService {
         for (const word of this._words) {
             if (word.cells[0] === cell && word !== this._selectedWord.word) {
 
-                return this.setSelectedWord(this._selectedWord, word, true);
+                return WordStatusManager.setSelectedWord(
+                    this._selectedWord, word, true, this.communicationService, this._id, this._nbPlayers);
             }
         }
         for (const word of this._words) {
             if (word.cells.indexOf(cell) !== -1 && word !== this._selectedWord.word) {
 
-                return this.setSelectedWord(this._selectedWord, word, true);
+                return WordStatusManager.setSelectedWord(
+                    this._selectedWord, word, true, this.communicationService, this._id, this._nbPlayers);
             }
         }
 
@@ -120,7 +86,7 @@ export class GridEventService {
         }
         event.stopPropagation();
 
-        return this.setSelectedWord(this._selectedWord, word, true);
+        return WordStatusManager.setSelectedWord(this._selectedWord, word, true, this.communicationService, this._id, this._nbPlayers);
     }
 
     public onKeyPress(event: KeyboardEvent): void {
