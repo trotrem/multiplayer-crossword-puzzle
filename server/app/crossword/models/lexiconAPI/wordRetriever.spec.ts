4bd51da6d217087dd846f4d3049b81333868396b
Laurence Duperron-Colbert <laurence.duperron-colbert@polymtl.ca>
2018-04-18 15:59:00 -0400

    Laurence DC: enlever tslint inutile

--- server/app/crossword/models/lexiconAPI/wordRetriever.spec.ts
+++ server/app/crossword/models/lexiconAPI/wordRetriever.spec.ts
@@ -10,7 +10,6 @@ let words: WordDictionaryData[] = [];
 describe("Test to see if wordRetrieve correctly create object of the word 'hall'.", () => {
 
     it("The word should be : 'hall'. ", async () => {
-        console.log("hi");
         words = await wordRetriever.getWordsWithDefinitions("hall", Difficulty.Easy);
         const word: string = words[0].word;
         const expectedWord: string = "hall";
@@ -34,8 +33,7 @@ describe("Test to see if wordRetrieve correctly create object of the word 'hall'
     it("The word 'hall' should be common. ", async () => {
         words = await wordRetriever.getWordsWithDefinitions("hall", Difficulty.Easy);
         const isCommon: boolean = words[0].isCommon;
-        // Fonction Chai ne finissant pas avec des parantheses
-        expect(isCommon).to.be.true; // tslint:disable-line
+        expect(isCommon).to.equal(true);
     });
 
 });
@@ -53,8 +51,7 @@ describe("test on Querry of the words 't?e' to see if it handles certain excepti
         words = await wordRetriever.getWordsWithDefinitions("t?e", Difficulty.Hard);
         const teeIndex: number = 2;
         const isCommon: boolean = words[teeIndex].isCommon;
-        // Fonction Chai ne finissant pas avec des parantheses
-        expect(isCommon).to.be.false; // tslint:disable-line
+        expect(isCommon).to.equal(false);
     });
 });
 
@@ -142,8 +139,7 @@ describe("Querry of 5 letter words to check specific constraint", () => {
 describe("Test on words with non alpha character", () => {
     it("When we query the word 3d, the array should be empty ", async () => {
         words = await wordRetriever.getWordsWithDefinitions("3d", Difficulty.Hard);
-        // Fonction Chai ne finissant pas avec des parantheses
-        expect(words).to.be.empty; // tslint:disable-line
+        expect(words.length).to.equal(0);
 
     });
 });
