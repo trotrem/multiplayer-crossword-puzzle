7c1a26168eb028c9be5c291349c6eadde4f9b266
Sarah Denis <sarah.denis@polymtl.ca>
2018-04-17 21:56:14 -0400

    Revert "Renommer grid-service en grid-manager, petits bugs reglées"
    This reverts commit e0ea8ba4b469ab79151dd9b4af6828dc9a359d4c.

--- client/src/app/crossword/grid-manage.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GridManager } from "./grid-manager.service";
-
-describe("GridService", () => {
-    it("should create an instance", () => {
-        expect(true).toBeTruthy();
-    });
-});
---
+++ client/src/app/crossword/grid-service.spec.ts
@@ -0,0 +1,7 @@
+import { GridService } from "./grid-service";
+
+describe("GridService", () => {
+    it("should create an instance", () => {
+        expect(new GridService()).toBeTruthy();
+    });
+});
