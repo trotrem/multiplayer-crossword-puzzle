03992dd49a86242f5b518da0e886ffd899abf68a
Sarah Denis <sarah.denis@polymtl.ca>
2018-04-18 23:23:08 -0400

    Message pour expliquer un disable de tslint

--- client/src/app/crossword/services/word-status-manager/word-status-manager.service.ts
+++ client/src/app/crossword/services/word-status-manager/word-status-manager.service.ts
@@ -34,7 +34,16 @@ export class WordStatusManagerService {
 
         return target.word;
     }
-    // TODO repare
+
+    /*Nous avons décider de disable ce ts-lint car il est beaucoup plus propre et plus simple que de
+    /*faire plusieurs conditions. Nous avons vérifié les raisons de ce tslint : plus difficile à maintenir
+    /*et la possiblité d'un typo entre les opérations booléennes et binaires. Dans le premier cas, comme
+    /*expliqué plus tôt, il est selon nous plus simple d'utiliser les opérations binaires. Pour le deuxième,
+    /*nous sommes bien au courant que ce ne sont pas des opérations booléennes mais bien des oéprations binaires.
+    /*Cette décision est réfléchi et donc aucune erreur que tslint tente d'éviter n'est possible. Merci de
+    /*prendre cette explication en considération.*/
+
+    // tslint:disable:no-bitwise
     private setWordSelectedState(target: SelectedWord, word: WordDescription, setSelected: boolean): void {
         for (const cell of word.cells) {
             cell.selectedBy = setSelected ?
